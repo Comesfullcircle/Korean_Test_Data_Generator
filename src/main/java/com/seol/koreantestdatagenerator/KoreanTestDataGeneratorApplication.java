78277229d0de8b75902a480a01e0dcdb7c1d6c40
윤설이 <olerlmin1234@gmail.com>
2025-01-06 18:12:07 +0900

    # 8 - 가짜 데이터 자료형 enum 설계
    가짜 데이터 타입은 DB 에는 저장 관리하지 않기로 함.
    따라서 타입을 용이하게 관리할 수 있는 enum 방식을 선택.
    enum 정보는 가짜 데이터 생성에 필요한 옵션 정보를 들고 있고, 이를 프론트에도 넘겨줄 수 있도록 하는 방법을 추가로 고민함.
    구현한 내용을 간단히 문자열 변환하여 테스트함.

--- src/main/java/com/seol/koreantestdatagenerator/KoreanTestDataGeneratorApplication.java
+++ src/main/java/com/seol/koreantestdatagenerator/KoreanTestDataGeneratorApplication.java
@@ -1,5 +1,6 @@
 package com.seol.koreantestdatagenerator;
 
+import com.seol.koreantestdatagenerator.domain.TableSchema;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
