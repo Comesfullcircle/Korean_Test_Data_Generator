bdd343092e27a570c0b08fcab3b335d821e3256b
윤설이 <olerlmin1234@gmail.com>
2025-01-06 14:52:29 +0900

    #6- 테이블 스키마, 스키마 필드 도메인 설계
    테이블 스키마와 스키마 필드를 다룰 때 필요한 데이터를 클래스로 설계함.
    기본적인 POJO 형태를 따름.

---
+++ src/main/java/com/seol/koreantestdatagenerator/domain/TableSchema.java
@@ -0,0 +1,18 @@
+package com.seol.koreantestdatagenerator.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+public class TableSchema {
+
+    private String schemaName;
+    private String userId;
+    private LocalDateTime exportedAt;
+
+}
