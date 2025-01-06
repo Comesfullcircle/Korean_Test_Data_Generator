6db2c728713519d77ad459e5318403a157c869de
윤설이 <olerlmin1234@gmail.com>
2025-01-06 23:34:23 +0900

    #40 - 실제 DB(MySql) 접근을 위한 프로퍼티 설정 및 테스트 설정 추가
    기존 프로필에서 부트 앱을 실행할 경우 실제 DB(mysql)에 접근할 수 있도록 datasource 정보를 변경함.
    그러나, 그대로 넣을 경우 암호, ID 등의 민감정보가 소스코드를 통해 깃헙에 유출되므로 시스템 환경변수 형식으로 추가.
    로컬에서 이 부분을 동작하게 하려면 별도로 시스템 환경변수를 적절히 세팅해 줘야 함
    
    기본 프로필은 이제 환경변수를 바라봄으로 환경변수 없이 테스트를 동작할 수 있게 기본 테스트에 'test' 프로필을 애노테이션으로 지정함

--- src/test/java/com/seol/koreantestdatagenerator/KoreanTestDataGeneratorApplicationTests.java
+++ src/test/java/com/seol/koreantestdatagenerator/KoreanTestDataGeneratorApplicationTests.java
@@ -2,7 +2,9 @@ package com.seol.koreantestdatagenerator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class KoreanTestDataGeneratorApplicationTests {
 
