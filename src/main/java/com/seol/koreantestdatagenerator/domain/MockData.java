cf024efb9e12b97abac199784bd8105ee0476e92
윤설이 <olerlmin1234@gmail.com>
2025-01-11 15:40:29 +0900

    # 12 - 도메인 클래스를 엔티티로 전환
    각 도메인이 실제로 JPA를 통해
    db에 접근할 수 있도록 엔티티 연관 정보를 추가함.

--- src/main/java/com/seol/koreantestdatagenerator/domain/MockData.java
+++ src/main/java/com/seol/koreantestdatagenerator/domain/MockData.java
@@ -1,16 +1,82 @@
 package com.seol.koreantestdatagenerator.domain;
 
 import com.seol.koreantestdatagenerator.domain.constant.MockDataType;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.Objects;
+
+/**
+ * 특정 {@link MockDataType}에 대응하는 가짜 데이터.
+ * 알고리즘으로 생성하지 않는 {@link MockDataType}의 경우, 이 가짜 데이터를 랜덤으로 뽑아 출력한다.
+ *
+ * @author seol
+ */
 @Getter
-@Setter
 @ToString
+@Table(uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"mockDataType", "mockDataValue"})
+})
+@Entity
 public class MockData {
 
-    private MockDataType mockDataType;
-    private String mockDataValue;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Setter @Column(nullable = false) private MockDataType mockDataType;
+    @Setter @Column(nullable = false) private String mockDataValue;
+
+    protected MockData() {}
+
+    public MockData(MockDataType mockDataType, String mockDataValue){
+        this.mockDataType = mockDataType;
+        this.mockDataValue = mockDataValue;
+    }
+
+    public static MockData of(MockDataType mockDataType, String mockDataValue){
+        return new MockData(mockDataType, mockDataValue);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (!(object instanceof MockData that)) return false;
+
+        if(getId() == null) {
+            return Objects.equals(this.getMockDataType(), that.getMockDataType()) &&
+                    Objects.equals(this.getMockDataValue(), that.getMockDataValue());
+        }
+
+        return Objects.equals(this.getId(), that.getId());
+
+      /*  if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        MockData mockData = (MockData) object;
+
+        if (id ==null) {
+            return Objects.equals(mockDataType, mockData.mockDataType) &&
+                    Objects.equals(mockDataValue, mockData.mockDataValue);
+        }
+
+        return Objects.equals(id, mockData.id);*/
+
+      //  return Objects.equals(id, mockData.id) &&
+      //          mockDataType == mockData.mockDataType &&
+     //           Objects.equals(mockDataValue, mockData.mockDataValue);
+    }
+
+    @Override
+    public int hashCode() {
+
+        if (id == null) {
+            return Objects.hash(mockDataType, mockDataValue);
+        }
+
+        return Objects.hash(id);
+
+        //return Objects.hash(id, mockDataType, mockDataValue);
+    }
 }
--- src/main/java/com/seol/koreantestdatagenerator/domain/TableSchema.java
+++ src/main/java/com/seol/koreantestdatagenerator/domain/TableSchema.java
@@ -1,18 +1,96 @@
 package com.seol.koreantestdatagenerator.domain;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 import java.time.LocalDateTime;
+import java.util.*;
 
+/**
+ * 단위 테이블 스키마 정보.
+ * 식별자({@link #userId})로 특정할 수 있는 회원이 소유한다.
+ *
+ * @author seol
+ */
 @Getter
-@Setter
-@ToString
-public class TableSchema {
+@ToString(callSuper = true)
+@Entity
+public class TableSchema extends AuditingFields{
 
-    private String schemaName;
-    private String userId;
-    private LocalDateTime exportedAt;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Setter private String schemaName;
+    @Setter private String userId;
+    @Setter private LocalDateTime exportedAt;
+
+    @ToString.Exclude
+    @OneToMany(mappedBy = "tableSchema", cascade = CascadeType.ALL, orphanRemoval = true)
+    private final Set<SchemaField> schemaFields = new LinkedHashSet<>();
+
+    protected TableSchema() {}
+
+    public TableSchema(String schemaName, String userId) {
+        this.schemaName = schemaName;
+        this.userId = userId;
+        this.exportedAt = null;
+    }
+
+    public static TableSchema of(String schemaName, String userId) {
+        return new TableSchema(schemaName, userId);
+    }
+
+    public void markExported(){
+        exportedAt = LocalDateTime.now();
+    }
+
+    public boolean isExpected(){
+        return exportedAt != null;
+    }
+
+    public void addSchemaField(SchemaField schemaField) {
+        schemaField.setTableSchema(this);
+        schemaFields.add(schemaField);
+    }
+
+    public void addSchemaFields(Collection<SchemaField> schemaFields) {
+        schemaFields.forEach(this::addSchemaField);
+    }
+
+    public void clearSchemaFields() {
+        schemaFields.clear();
+    }
+
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (!(object instanceof TableSchema that)) return false;
+
+        if (that.getId() == null) {
+            return Objects.equals(this.getSchemaName(), that.getSchemaName() )&&
+                    Objects.equals(this.getUserId(), that.getUserId()) &&
+                    Objects.equals(this.getExportedAt(), that.getExportedAt()) &&
+                    Objects.equals(this.getSchemaFields(), that.getSchemaFields());
+        }
+
+        return Objects.equals(this.getId(), that.getId());
+
+       // if (object == null || getClass() != object.getClass()) return false;
+       // TableSchema that = (TableSchema) object;
+       // return Objects.equals(id, that.id) && Objects.equals(schemaName, that.schemaName) && Objects.equals(userId, that.userId) && Objects.equals(exportedAt, that.exportedAt) && Objects.equals(schemaFields, that.schemaFields);
+    }
+
+    @Override
+    public int hashCode() {
+        if (getId() == null) {
+            return Objects.hash(this.getSchemaName(), this.getUserId(), this.getExportedAt(), this.getSchemaFields());
+        }
+        return Objects.hash(getId());
+
+        //return Objects.hash(id, schemaName, userId, exportedAt, schemaFields);
+    }
 }
