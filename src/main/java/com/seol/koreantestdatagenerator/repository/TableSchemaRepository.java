e73331104b0167e7a79bf09f09f963fc8c864f29
윤설이 <olerlmin1234@gmail.com>
2025-01-12 19:53:16 +0900

    #15- TableSchemaRepository 필요없는 import 정리
    TableSchemaRepository.java에서 필요없는 import 정리함

--- src/main/java/com/seol/koreantestdatagenerator/repository/TableSchemaRepository.java
+++ src/main/java/com/seol/koreantestdatagenerator/repository/TableSchemaRepository.java
@@ -4,7 +4,6 @@ import com.seol.koreantestdatagenerator.domain.TableSchema;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
