68f391485fd8b8a7004110070b1107a63e0bd49f
윤설이 <olerlmin1234@gmail.com>
2025-01-17 12:56:28 +0900

    #18 - 테이블 스키마 뷰에서 필요한 추가 dto 구현
    테이블 스키마 뷰에서
    파일 출력 버튼을 고려한 데이터 구조를 추가함

---
+++ src/main/java/com/seol/koreantestdatagenerator/domain/constant/ExportFileType.java
@@ -0,0 +1,5 @@
+package com.seol.koreantestdatagenerator.domain.constant;
+
+public enum ExportFileType {
+    CSV, TSV, JSON, SQL_INSERT;
+}
---
+++ src/main/java/com/seol/koreantestdatagenerator/dto/request/TableSchemaExportRequest.java
@@ -0,0 +1,35 @@
+package com.seol.koreantestdatagenerator.dto.request;
+
+import com.seol.koreantestdatagenerator.domain.constant.ExportFileType;
+import com.seol.koreantestdatagenerator.dto.TableSchemaDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.aspectj.apache.bcel.classfile.Module;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+//TODO: 후에 record 방식으로 쓸 수 있는지 검토하기
+@NoArgsConstructor
+@AllArgsConstructor(staticName = "of")
+@Data
+public class TableSchemaExportRequest {
+
+    private String schemaName;
+    private Integer rowCount;
+    private ExportFileType fileType;
+    private List<SchemaFieldRequest> schemaFields;
+
+    public TableSchemaDto toDto(String userId) {
+        return TableSchemaDto.of(
+                schemaName,
+                userId,
+                null,
+                schemaFields.stream()
+                        .map(SchemaFieldRequest::toDto)
+                        .collect(Collectors.toUnmodifiableSet())
+        );
+    }
+
+}
