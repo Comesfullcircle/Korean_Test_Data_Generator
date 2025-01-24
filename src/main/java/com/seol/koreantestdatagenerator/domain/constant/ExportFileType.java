4d0ce13ddefea88139ee6a3900aee4534ed87ca8
윤설이 <olerlmin1234@gmail.com>
2025-01-24 14:18:28 +0900

    #29 - 우선순위
    시간관계상 json, sql 부분 후에 처리

--- src/main/java/com/seol/koreantestdatagenerator/domain/constant/ExportFileType.java
+++ src/main/java/com/seol/koreantestdatagenerator/domain/constant/ExportFileType.java
@@ -3,7 +3,7 @@ package com.seol.koreantestdatagenerator.domain.constant;
 public enum ExportFileType {
     CSV,
     TSV,
-    JSON,
-    SQL_INSERT,
+  //  JSON, //TODO:후에 할 것
+  //  SQL_INSERT, //TODO:후에 할 것
     ;
 }
