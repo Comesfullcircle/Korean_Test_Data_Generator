fb52a6297ca216618f3d5905c423962cd19b6674
윤설이 <olerlmin1234@gmail.com>
2025-01-25 15:42:27 +0900

    #30 - 가짜 데이터 생성기를 파일 출력기에 연결
    이것으로 가짜 데이터 생성부터 파일 출력기를 거쳐 서비스 로직 -> 컨트롤러까지 기본 기능이 모두 구현 및 연결 됨

--- src/main/java/com/seol/koreantestdatagenerator/service/exporter/TSVFileExporter.java
+++ src/main/java/com/seol/koreantestdatagenerator/service/exporter/TSVFileExporter.java
@@ -3,6 +3,7 @@ package com.seol.koreantestdatagenerator.service.exporter;
 import com.seol.koreantestdatagenerator.domain.constant.ExportFileType;
 import com.seol.koreantestdatagenerator.dto.SchemaFieldDto;
 import com.seol.koreantestdatagenerator.dto.TableSchemaDto;
+import com.seol.koreantestdatagenerator.service.generator.MockDataGeneratorContext;
 import org.springframework.stereotype.Component;
 
 import java.util.Comparator;
@@ -11,6 +12,10 @@ import java.util.stream.IntStream;
 
 @Component
 public class TSVFileExporter extends DelimiterBasedFileExporter implements MockDataFileExporter {
+
+    public TSVFileExporter(MockDataGeneratorContext mockDataGeneratorContext) {
+        super(mockDataGeneratorContext);
+    }
 
     @Override
     public String getDelimiter() {
--- src/test/java/com/seol/koreantestdatagenerator/service/exporter/TSVFileExporterTest.java
+++ src/test/java/com/seol/koreantestdatagenerator/service/exporter/TSVFileExporterTest.java
@@ -3,18 +3,30 @@ package com.seol.koreantestdatagenerator.service.exporter;
 import com.seol.koreantestdatagenerator.domain.constant.MockDataType;
 import com.seol.koreantestdatagenerator.dto.SchemaFieldDto;
 import com.seol.koreantestdatagenerator.dto.TableSchemaDto;
+import com.seol.koreantestdatagenerator.service.generator.MockDataGeneratorContext;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
 
 @DisplayName("[Logic] TSV 파일 출력하기 테스트")
+@ExtendWith(MockitoExtension.class)
 class TSVFileExporterTest {
 
-    private TSVFileExporter sut = new TSVFileExporter();
+    @InjectMocks private TSVFileExporter sut;
+
+    @Mock private MockDataGeneratorContext mockDataGeneratorContext;
 
     @DisplayName("테이블 스키마 정보와 행 수가 주어지면, TSV 형식의 문자열을 생성한다.")
     @Test
@@ -33,6 +45,7 @@ class TSVFileExporterTest {
                 )
         );
         int rowCount = 10;
+        given(mockDataGeneratorContext.generate(any(), any(), any(), any())).willReturn("test-value");
 
         //When
         String result = sut.export(dto, rowCount);
@@ -40,6 +53,8 @@ class TSVFileExporterTest {
         //Then
         System.out.println(result);
         assertThat(result).startsWith("id\tname\tage\tcar\tcreated_at");
+        then(mockDataGeneratorContext).should(times(rowCount * dto.schemaFields().size())).generate(any(), any(), any(), any());
+
 
     }
 }
